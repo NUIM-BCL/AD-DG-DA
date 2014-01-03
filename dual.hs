9d9b52c1645c5631019b7784ad13fb5e730d8599
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2014-01-03 11:51:24 +0100

    make dual a proper module

--- dual.hs
+++ dual.hs
@@ -1,18 +1,39 @@
 {-# LANGUAGE UnicodeSyntax #-}
-import Prelude.Unicode
 
 {- Minimally Fart around with Dual Numbers -}
 
-data Dual a = Dual a a
-            deriving (Ord, Show, Eq)
+module Numeric.Dual -- (Dual)
+where
 
-instance Num a ⇒ Num (Dual a) where
+import Prelude.Unicode
+
+-- | The 'Dual' type is a concrete representation of a Dual number,
+-- meaning a number augmented with a derivatives (Clifford, 1873).
+-- These can be regarded as truncated power series.  The intended use
+-- is for overloading arithmetic in order perform a nonstandard
+-- interpretation of numeric code, for doing forward-mode automatic
+-- differeniation (Wengert, 1964).  The "tag" allows branding, to
+-- avoid perturbation confusion (Siskind and Pearlmutter, 2008).
+data Dual tag a = Dual a a
+            deriving (Read, Show)
+
+lift ∷ Num a ⇒ a → Dual tag a
+lift = flip Dual 0
+
+instance Eq a ⇒ Eq (Dual tag a) where
+  Dual x _ == Dual y _ = x ≡ y -- Can use ≡ on RHS but must use == on LHS.
+
+instance Ord a ⇒ Ord (Dual tag a) where
+  compare (Dual x _) (Dual y _) = compare x y
+
+instance Num a ⇒ Num (Dual tag a) where
   Dual x x' + Dual y y' = Dual (x+y) (x'+y')
-  Dual x x' * Dual y y' = Dual (x⋅y) (x⋅y'+x'⋅y)
-  signum (Dual x x') = Dual (signum x) (x'⋅0)
+  Dual x x' * Dual y y' = Dual (x⋅y) (x⋅y'+x'⋅y) -- Can use ⋅ on RHS but must use * on LHS.
+  signum (Dual x _) = lift $ signum x
+  -- signum (Dual x x') = Dual (signum x) (x'⋅0)
   abs x = x ⋅ signum x
-  fromInteger = flip Dual 0 ∘ fromInteger
+  fromInteger = lift ∘ fromInteger
 
-instance Fractional a ⇒ Fractional (Dual a) where
+instance Fractional a ⇒ Fractional (Dual tag a) where
   recip (Dual x x') = Dual (recip x) (-x'/x^(2∷Int))
-  fromRational = flip Dual 0 ∘ fromRational
+  fromRational = lift ∘ fromRational
