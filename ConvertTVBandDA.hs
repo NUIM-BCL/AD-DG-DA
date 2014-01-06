f0a8a8853f3e7bbb65e241f322f28d82b0872d59
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2014-01-06 18:06:26 +0100

    break out ConvertTVBandDA into its own module

---
+++ ConvertTVBandDA.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module ConvertTVBandDA (ConvertTVBandDA, toDA, toTVB)
+where
+
+import Prelude.Unicode
+import DifferentialAlgebra (DA)
+import qualified DifferentialAlgebra as DA (lift, primal)
+import TangentVectorBundle (TVB)
+import Numeric.Dual (Dual)
+
+
+-- Need to define converters DA to/from TVB, in order to implement DG
+-- via DA.
+
+class ConvertTVBandDA tag a a' ta da ba
+ | tag a→a' ta da ba,
+   ta→tag a,
+   ba→tag a
+ where
+  toDA ∷  (TVB tag a a' ta, DA tag a da ba) ⇒ ta→ba
+  toTVB ∷  (TVB tag a a' ta, DA tag a da ba) ⇒ ba→ta
+
+instance ConvertTVBandDA tag Double Double (Dual tag Double) Double (Dual tag Double)
+ where
+  toDA = id
+  toTVB = id
+
+-- instance (Num a, TVB tag a a (Dual tag a), DA tag a a (Dual tag a))
+--          ⇒ ConvertTVBandDA tag a a (Dual tag a) a (Dual tag a) where
+--   toDA = id
+--   toTVB = id
+
+instance (TVB tag a a' ta,
+          DA tag a da ba,
+          ConvertTVBandDA tag a a' ta da ba,
+          TVB tag b b' tb,
+          DA tag b db bb,
+          ConvertTVBandDA tag b b' tb db bb)
+         ⇒
+         ConvertTVBandDA tag (a→b) (a→b') (a→tb) (da→db) (ba→bb)
+ where
+  toTVB f = toTVB ∘ f ∘ DA.lift
+  toDA f = toDA ∘ f ∘ DA.primal -- *unsafe* unless tangent is zero
+
+instance (TVB tag a a' ta,
+          DA tag a da ba,
+          ConvertTVBandDA tag a a' ta da ba,
+          TVB tag b b' tb,
+          DA tag b db bb,
+          ConvertTVBandDA tag b b' tb db bb)
+         ⇒
+         ConvertTVBandDA tag (a,b) (a',b') (ta,tb) (da,db) (ba,bb)
+ where
+  toTVB (x,y) = (toTVB x, toTVB y)
+  toDA (x,y) = (toDA x, toDA y)
+
+instance (TVB tag a a' ta,
+          DA tag a da ba,
+          ConvertTVBandDA tag a a' ta da ba)
+         ⇒
+         ConvertTVBandDA tag [a] [a'] [ta] [da] [ba]
+ where
+  toTVB = fmap toTVB
+  toDA = fmap toDA
+
+instance (TVB tag a a' ta,
+          DA tag a da ba,
+          ConvertTVBandDA tag a a' ta da ba)
+         ⇒
+         ConvertTVBandDA tag (Maybe a) (Maybe a') (Maybe ta) (Maybe da) (Maybe ba)
+ where
+  toTVB = fmap toTVB
+  toDA = fmap toDA
+
+instance (TVB tag a a' ta,
+          DA tag a da ba,
+          ConvertTVBandDA tag a a' ta da ba,
+          TVB tag b b' tb,
+          DA tag b db bb,
+          ConvertTVBandDA tag b b' tb db bb)
+         ⇒
+         ConvertTVBandDA tag (Either a b)
+         (Either a' b') (Either ta tb)
+         (Either da db) (Either ba bb)
+ where
+  toTVB (Left  da) = Left  (toTVB da)
+  toTVB (Right db) = Right (toTVB db)
+  toDA (Left  ta) = Left  (toDA ta)
+  toDA (Right tb) = Right (toDA tb)
--- Diff.hs
+++ Diff.hs
@@ -1,95 +1,14 @@
 {-# LANGUAGE UnicodeSyntax #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE UndecidableInstances #-}
 
 module Diff (ConvertTVBandDA, diff, pushforward, pushforward_)
 where
 
 import Prelude.Unicode
 import DifferentialAlgebra (DA)
-import qualified DifferentialAlgebra as DA (lift, primal)
+import qualified DifferentialAlgebra as DA (lift)
 import TangentVectorBundle (TVB)
 import qualified TangentVectorBundle as TVB (tangent, bundle)
-import Numeric.Dual (Dual)
-
--- Need to define converters DA to/from TVB, in order to implement DG
--- via DA.
-
-class ConvertTVBandDA tag a a' ta da ba
- | tag a→a' ta da ba,
-   ta→tag a,
-   ba→tag a
- where
-  fromTVBtoDA ∷  (TVB tag a a' ta, DA tag a da ba) ⇒ ta→ba
-  fromDAtoTVB ∷  (TVB tag a a' ta, DA tag a da ba) ⇒ ba→ta
-
-instance ConvertTVBandDA tag Double Double (Dual tag Double) Double (Dual tag Double)
- where
-  fromTVBtoDA = id
-  fromDAtoTVB = id
-
--- instance (Num a, TVB tag a a (Dual tag a), DA tag a a (Dual tag a))
---          ⇒ ConvertTVBandDA tag a a (Dual tag a) a (Dual tag a) where
---   fromTVBtoDA = id
---   fromDAtoTVB = id
-
-instance (TVB tag a a' ta,
-          DA tag a da ba,
-          ConvertTVBandDA tag a a' ta da ba,
-          TVB tag b b' tb,
-          DA tag b db bb,
-          ConvertTVBandDA tag b b' tb db bb)
-         ⇒
-         ConvertTVBandDA tag (a→b) (a→b') (a→tb) (da→db) (ba→bb)
- where
-  fromDAtoTVB f = fromDAtoTVB ∘ f ∘ DA.lift
-  fromTVBtoDA f = fromTVBtoDA ∘ f ∘ DA.primal -- *unsafe* unless tangent is zero
-
-instance (TVB tag a a' ta,
-          DA tag a da ba,
-          ConvertTVBandDA tag a a' ta da ba,
-          TVB tag b b' tb,
-          DA tag b db bb,
-          ConvertTVBandDA tag b b' tb db bb)
-         ⇒
-         ConvertTVBandDA tag (a,b) (a',b') (ta,tb) (da,db) (ba,bb)
- where
-  fromDAtoTVB (x,y) = (fromDAtoTVB x, fromDAtoTVB y)
-  fromTVBtoDA (x,y) = (fromTVBtoDA x, fromTVBtoDA y)
-
-instance (TVB tag a a' ta,
-          DA tag a da ba,
-          ConvertTVBandDA tag a a' ta da ba)
-         ⇒
-         ConvertTVBandDA tag [a] [a'] [ta] [da] [ba]
- where
-  fromDAtoTVB = fmap fromDAtoTVB
-  fromTVBtoDA = fmap fromTVBtoDA
-
-instance (TVB tag a a' ta,
-          DA tag a da ba,
-          ConvertTVBandDA tag a a' ta da ba)
-         ⇒
-         ConvertTVBandDA tag (Maybe a) (Maybe a') (Maybe ta) (Maybe da) (Maybe ba)
- where
-  fromDAtoTVB = fmap fromDAtoTVB
-  fromTVBtoDA = fmap fromTVBtoDA
-
-instance (TVB tag a a' ta,
-          DA tag a da ba,
-          ConvertTVBandDA tag a a' ta da ba,
-          TVB tag b b' tb,
-          DA tag b db bb,
-          ConvertTVBandDA tag b b' tb db bb)
-         ⇒
-         ConvertTVBandDA tag (Either a b) (Either a' b') (Either ta tb) (Either da db) (Either ba bb)
- where
-  fromDAtoTVB (Left  da) = Left  (fromDAtoTVB da)
-  fromDAtoTVB (Right db) = Right (fromDAtoTVB db)
-  fromTVBtoDA (Left  ta) = Left  (fromTVBtoDA ta)
-  fromTVBtoDA (Right tb) = Right (fromTVBtoDA tb)
+import ConvertTVBandDA (ConvertTVBandDA, toDA, toTVB)
 
 -- ACTUAL DERIVATIVE-TAKING OPERATORS!
 -- (Well, scaffolding and types.)
@@ -104,7 +23,7 @@ pushforward ∷ (TVB tag a a' ta, DA tag a da ba,
 -- for the function argument, so that it can actually be lifted.
 -- Which requires (among other things) RankNTypes.
 
-pushforward f = fromDAtoTVB ∘ DA.lift f ∘ fromTVBtoDA
+pushforward f = toTVB ∘ DA.lift f ∘ toDA
 
 pushforward_ ∷ (TVB tag a a' ta,
                 TVB tag b b' tb)
